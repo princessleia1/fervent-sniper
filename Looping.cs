9ca4fb8e1ebe5e84d902d8c99264bcda3b0c6e33
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-07-11 17:22:40 +1000

    Add do while loops

---
+++ Looping.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace Looping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While loop
+            int x = 0;
+            while (x < 10)
+            {
+                Console.WriteLine("Hello");
+                x++;
+            }
+
+            // For loop
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello Again");
+            }
+
+            Console.WriteLine("Enter an amount of times to print hello: ");
+            int amount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < amount; i++)
+            {
+                Console.WriteLine("For Loop");
+            }
+
+            // Do while loop
+            int z = 5; // Executes at least one time
+            do
+            {
+                Console.WriteLine("Do While Loop");
+                z++;
+            }
+            while (z > 6) ;
+
+            // While loop
+            Console.WriteLine("Hello");
+            string response = Console.ReadLine();
+
+            while (response == "again")
+            {
+                Console.WriteLine("Hello");
+                response = Console.ReadLine();
+            }
+
+            // Do while loop
+            // Condition comes first
+            string responseAgain;
+            do
+            {
+                Console.WriteLine("Hello");
+                responseAgain = Console.ReadLine();
+            } while (responseAgain == "Again");
+        }
+    }
+}
