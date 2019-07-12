a43ade7a114dc3e88ed0fac5b6c0b37448645768
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-07-12 12:00:28 +1000

    Add SumsIntegers.cs program

---
+++ SumsIntegers.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace SumsIntegers
+{
+    class Program
+    {
+        /* Write a console-based application that sums the integers from 1 to 50.
+         */
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 50; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("The Sum is {0}.", sum);
+        }
+    }
+}
