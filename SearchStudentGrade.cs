2d0576022c74f85b29ac1c274e5a845617d55133
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-07-15 11:23:58 +1000

    Add search student grade use parallel array

---
+++ SearchStudentGrade.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace SearchStudentGrade
+{
+    class Program
+    {
+        /* Write a console application that asks a user to enter a student's name.
+         * If the student if found indicate that they are found and their associated grade.
+         * Indicate to the user if the student is not found.
+         * Use a paraelle array.
+         */
+
+        static void Main(string[] args)
+        {
+            // String array
+            string[] students = { "Han", "Leia", "Obi-Wan", "Luke" };
+            int[] grades = { 77, 50, 78, 99 };
+
+            Console.WriteLine("Enter Students Name to Search for:");
+            string studentName = Console.ReadLine();
+
+            // Logic
+            bool contains = false;
+            int studentGrade = 0;
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (studentName == students[i])
+                {
+                    contains = true;
+                    studentGrade = grades[i];
+                    break;
+                }
+            }
+            if (contains == true)
+            {
+                Console.WriteLine("The student is FOUND");
+                Console.WriteLine("The student's Grade is {0}", studentGrade);
+            }
+            else
+            {
+                 Console.WriteLine("The student is NOT FOUND");
+            }
+        }
+    }
+}
