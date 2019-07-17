bc7c114c7f64e730a3f611bcf4316ac62b1637d6
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-07-17 12:47:07 +1000

    Add simple sum method program

---
+++ SimpleSumMethod.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace SimpleSum
+{
+    class Program
+    {
+        /* Create a method named Sum() that accepts a number of integer parameters and displays their sum.
+         * Write a Maint() method that demonstrates the Sum() method works correctly
+         * When passed one, three or five integers, or an array of ten integers.
+         */
+        static void Main(string[] args)
+        {
+            Sum(5);
+            Sum(5, 10, 15);
+            Sum(5, 10, 15, 20, 25);
+
+            int[] myArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            Sum(myArray);
+        }
+
+        public static void Sum(params int[] myArray)
+        {
+            int sum = 0;
+            for(int i = 0; i < myArray.Length; i++)
+            {
+                sum += myArray[i];                
+            }
+            Console.WriteLine("The Sum is {0}", sum);
+        }
+    }
+}
