c1fb354ad34ac0d9a47950ee2615ea21bbf2c6dd
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-07-22 11:44:23 +1000

    Add object to method program

--- ObjectToMethod/Program.cs
+++ ObjectToMethod/Program.cs
@@ -11,10 +11,19 @@ namespace PassObjectsToMethods
         static void Main(string[] args)
         {
             Person p1 = new Person("Leia", 20);
-            Console.WriteLine("BEFORE: {0}, {1}",p1.Name,p1.Age);
+            Console.WriteLine("BEFORE: {0}, {1}", p1.Name, p1.Age);
             ChangePerson(p1);
-            Console.WriteLine("AFTER: {0}, {1}",p1.Name,p1.Age);
+            Console.WriteLine("AFTER: {0}, {1}", p1.Name, p1.Age);
+
+            // Pass Reference type data change
+            Person p2 = p1; // Exact place in memory
+            p2.Name = "TEST";
+            Console.WriteLine("AFTER ALTER 2: {0}, {1}", p1.Name, p1.Age);
         }
+        // Expected output:
+        // BEFORE: Leia, 20
+        // AFTER: Han, 50
+        // AFTER: TEST, 50
 
         // Function to manipulate the data, change the original data
         public static void ChangePerson(Person p1)
